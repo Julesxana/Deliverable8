6d0346a68614c8b5aec5a9f3baa2216a26446d10
Juliana Dunlap <59933088+Julesxana@users.noreply.github.com>
2020-03-10 17:00:50 -0400

    Add files via upload

---
+++ WildcatUniversityData/Course.sql
@@ -0,0 +1,7 @@
+﻿--create table called course
+CREATE TABLE [dbo].[Course] (
+    [CourseID] INT           IDENTITY (1, 1) NOT NULL,
+    [Title]    NVARCHAR (50) NULL,
+    [Credits]  INT           NULL,
+    PRIMARY KEY CLUSTERED ([CourseID] ASC)
+)
